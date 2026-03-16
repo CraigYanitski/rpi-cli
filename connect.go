b2dccff7c8c666eef1496f63fa0c9bb1bd91c2b0
Craig Yanitski <yanitski@ph1.uni-koeln.de>
2026-03-16 12:43:39 -0600

    minimize break in loading spinner

--- connect.go
+++ connect.go
@@ -162,7 +162,6 @@ func (cfg *apiConfig) rpiConnect() {
 	resp.Body.Close()
 
 	// get available devices
-	s.Stop()  // stop spinner for printing
 	devices := [][]string{}
 	pattern := regexp.MustCompile(devicesPattern)
 	matches := pattern.FindAllStringSubmatch(string(body), -1)
@@ -171,7 +170,12 @@ func (cfg *apiConfig) rpiConnect() {
 	}
 
 	// select device
-	deviceURL := getDeviceURL(devices)
+	s.Stop()  // stop spinner for printing
+	deviceName, deviceURL := getDeviceURL(devices)
+
+	// update spinner description
+	s.Suffix = fmt.Sprintf(" Waiting for response from %s...", deviceName)
+	s.Start()
 
 	r, err = http.NewRequest(
 		"GET",
@@ -229,10 +233,6 @@ func (cfg *apiConfig) rpiConnect() {
 		log.Fatal("Unable to collect device information")
 	}
 
-	// update spinner description
-	s.Suffix = fmt.Sprintf(" Waiting for response from %s...", deviceInfo.device.Name)
-	s.Start()
-
 	sessionToken := getSessionToken(string(body))
 	if sessionToken != "" {
 		deviceInfo.csrfToken = sessionToken
@@ -275,7 +275,7 @@ func (cfg *apiConfig) rpiConnect() {
 
 	// register data channel handlers
 	peerConnection.OnDataChannel(func(d *webrtc.DataChannel) {
-		fmt.Printf("Data channel (%d) '\"%s\" - %d' open\n", d.ID(), d.Label(), d.ID())
+		fmt.Printf("Data channel \"%s\" (id: %d)\n", d.Label(), *d.ID())
 
 		//if d.Label() == "shell" {
 		//	d.OnOpen(func() {
@@ -325,7 +325,7 @@ func (cfg *apiConfig) rpiConnect() {
 
 	// register shell data channel
 	shellChannel.OnOpen(func() {
-		fmt.Printf("Data channel (%d) '\"%s\" - %d' open\n", *shellChannel.ID(), shellChannel.Label(), shellChannel.ID())
+		fmt.Printf("Data channel \"%s\" (id: %d)\n", shellChannel.Label(), *shellChannel.ID())
 
 		// detach data channel
 		raw, err := shellChannel.Detach()
@@ -386,134 +386,15 @@ func (cfg *apiConfig) rpiConnect() {
 	select{}
 }
 
-func (cfg *apiConfig) createDeviceSession(offer webrtc.SessionDescription) {
-	spdData := encode(&offer)
-	r, err := http.NewRequest(
-		"POST",
-		fmt.Sprintf("https://connect.raspberrypi.com/devices/%s/connections", cfg.deviceInfo.device.Id),
-		bytes.NewBuffer([]byte(spdData)),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	setHeader(
-		r,
-		"application/octet-stream",
-		"https://connect.raspberrypi.com",
-		fmt.Sprintf("https://connect.raspberrypi.com/devices/%s/remote-shell-session", cfg.deviceInfo.device.Id),
-	)
-	r.Header.Set("X-CSRF-Token", cfg.deviceInfo.csrfToken)
-
-	resp, err := cfg.client.Do(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if resp.StatusCode >= 400 {
-		log.Fatalf("Failed to create device session: %s\n", resp.Status)
-	}
-	location := resp.Header.Get("location")
-	if location == "" {
-		log.Fatal("Failed to create device session: No location in response\n")
-	}
-
-	sdpResponse := SDPResponse{}
-
-	for {
-		time.Sleep(500 * time.Millisecond)
-
-		r, err := http.NewRequest(
-			"GET",
-			"https://connect.raspberrypi.com" + location,
-			nil,
-		)
-		if err != nil {
-			log.Fatal(err)
-		}
-		setHeader(
-			r,
-			"",
-			"https://connect.raspberrypi.com",
-			fmt.Sprintf(
-				"https://connect.raspberrypi.com/devices/%s/remote-shell-session", 
-				cfg.deviceInfo.device.Id,
-			),
-		)
-		r.Header.Set("Accept", "*/*")
-		r.Header.Set("Priority", "u=4")
-		r.Header.Set("Sec-Fetch-Dest", "empty")
-		r.Header.Set("Sec-Fetch-Mode", "cors")
-		r.Header.Del("Content-Type")
-		r.Header.Del("Origin")
-
-		resp, err = cfg.client.Do(r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if resp.StatusCode >= 400 {
-			log.Fatalf("Error checking client SDP status: %s\n", resp.Status)
-		}
-
-		body, err := httpdecompressor.ReadAll(resp)
-		if err != nil {
-			log.Fatal(err)
-		}
-		resp.Body.Close()
-
-		err = json.Unmarshal(body, &sdpResponse)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if sdpResponse.Answer != nil {
-			break
-		}
-		sdpResponse = SDPResponse{}
-	}
-
-	// fmt.Printf("Accept: %s\n", sdpResponse.Answer.Sdp)
-
-	cfg.sdpChan <- sdpResponse.Answer.Sdp
-}
-
-func encode(sdp *webrtc.SessionDescription) []byte {
-	b, err := json.Marshal(sdp)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return b //base64.StdEncoding.EncodeToString(b)
-}
-
-func getSessionInformation(body string) []string {
-	pattern := regexp.MustCompile(
-		`(?s).*data-controller="([^"]*)"` +
-		`.*data-shell-session-id-value="([^"]*)"` +
-		`.*data-shell-device-value="([^"]*)"` +
-		`.*data-shell-ice-configuration-value="([^"]*)"` +
-		`.*`,
-	)
-	matches := pattern.FindAllStringSubmatch(body, -1)
-	if len(matches) == 1 {
-		return matches[0]
-	}
-	return nil
-}
-
-func getSessionToken(body string) string {
-	pattern := regexp.MustCompile(`name="csrf-token" content="([^"]*)"`)
-	matches := pattern.FindAllStringSubmatch(body, -1)
-	if len(matches) == 1 {
-		return matches[0][1]
-	}
-	return ""
-}
-
-func getDeviceURL(devices [][]string) string {
+func getDeviceURL(devices [][]string) (string, string) {
 	if devices == nil {
-		return ""
+		return "", 
+			""
 	}
 
 	if len(devices) == 1 {
-		return fmt.Sprintf("https://connect.raspberrypi.com/devices/%s/remote-shell-session", devices[0][0])
+		return devices[0][1], 
+		fmt.Sprintf("https://connect.raspberrypi.com/devices/%s/remote-shell-session", devices[0][0])
 	}
 
 	fmt.Println("\nDevices\n-------")
@@ -544,6 +425,7 @@ func getDeviceURL(devices [][]string) string {
 		}
 	}
 
-	return fmt.Sprintf("https://connect.raspberrypi.com/devices/%s/remote-shell-session", devices[id-1][0])
+	return devices[id-1][1], 
+		fmt.Sprintf("https://connect.raspberrypi.com/devices/%s/remote-shell-session", devices[id-1][0])
 }
 
